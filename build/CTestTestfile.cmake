ed1fe6c197ea447469f19d27e6a7167f65d51e68
nm-bda <F20200660@pilani.bits-pilani.ac.in>
2021-07-30 20:16:39 +0530

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/suramya/catkin_ws/src
+# Build directory: /home/suramya/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("robot_setup_tf")
+subdirs("turtlebot3_simulations/turtlebot3_fake")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/suramya/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/suramya/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/suramya/catkin_ws/devel;/opt/ros/melodic' != '' else []
